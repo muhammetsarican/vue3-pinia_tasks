eb83125dc9d9a889d7a21999cba574985c61b39a
Muhammet Sarıcan <muhammetsarican@gmail.com>
2025-04-14 14:45:14 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <!-- header - start -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="" />
+      <h1>Pinia Tasks</h1>
+    </header>
+    <!-- header - end -->
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
